d59531077653b8fc85b81b818a1b797515ba164c
Evgeny Nikolaev <e.nikolaev@uplab.ru>
2021-07-28 20:42:45 +0300

    js

---
+++ 3_difficult_js/1_prototype/js.js
@@ -0,0 +1,37 @@
+/*
+Прототип - это отдельный объект, который присутствует у родительских объектов, и вызывается по цепочке сверху вниз.
+На самом деле объект создается при помощи new Object (Object - самый глобальный, верхний), даже если использовать литерал
+
+	var person = {
+		name: 'Maxim',
+		age: 25,
+		greet: function () {
+			console.log('Greet');
+		}
+	};
+*/
+
+const person = new Object({
+	name: 'Maxim',
+	age: 25,
+	greet: function () {
+		console.log('Greet');
+	}
+});
+
+
+//расширили базовый прототип, и добавили в него метод (prototype - св-во у ф-ии конструктор)
+Object.prototype.sayHello = function () {
+	console.log('sayHello from Object.prototype');
+};
+
+person.sayHello();
+
+
+//создаем объект lena используя (на основе) прототип person
+const lena = Object.create(person);
+lena.name = 'Elena';
+lena.sayHello();
+
+
+let str = new String('i am string'); //можем в консоли посмотреть какие свойства методы есть у строки и цепочку прототипов
